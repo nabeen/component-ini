67ecca3ed1bc5ea557cdd2c9dc1b674b0beb0115
nabeen <watanabe_kenichiro@hasigo.co.jp>
2017-07-28 19:00:20 +0900

    シングルクォートで吐くように修正

--- src/IniWriter.php
+++ src/IniWriter.php
@@ -113,7 +113,7 @@ class IniWriter
             return (int) $value;
         }
         if (is_string($value)) {
-            return "\"$value\"";
+            return "'$value'";
         }
         return $value;
     }
